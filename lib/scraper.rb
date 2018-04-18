912bfaf2a2591328b14288d86588471dc2dc4049
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-18 00:12:43 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,14 +24,13 @@ def get_courses
 end
 
 
-    def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
+def make_courses
+  self.get_courses.each do |post|
+  course = Course.new
+  course.title = post.css("h2").text
+  course.schedule = post.css(".date").text
+  course.description = post.css("p").text
+end
+end
   end
 end
